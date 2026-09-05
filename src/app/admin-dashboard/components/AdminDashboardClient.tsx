702034de9208338ed42d76680e5c47eaada1de46
Rjtangpos <rjtangpos39@gmail.com>
2026-09-05 21:41:25 +0800

    fix

--- src/app/admin-dashboard/components/AdminDashboardClient.tsx
+++ src/app/admin-dashboard/components/AdminDashboardClient.tsx
@@ -3,7 +3,7 @@
 
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import AppLogo from '../../components/ui/AppLogo';
+import AppLogo from '../../../components/ui/AppLogo';
 import Link from 'next/link';
 import UploadSection from './UploadSection';
 import VersionsTable from './VersionsTable';
